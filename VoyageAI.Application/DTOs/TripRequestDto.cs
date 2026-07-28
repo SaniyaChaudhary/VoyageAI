6251d3f96d5ebaca12ff9767278ce85ec16874ad
Saniya <saniyach334@gmail.com>
2026-07-28 17:26:55 +0800

    Add trip request DTO for travel planning

---
+++ VoyageAI.Application/DTOs/TripRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class TripRequestDto
+{
+    public string Destination { get; set; } = string.Empty;
+
+    public int DurationDays { get; set; }
+
+    public decimal Budget { get; set; }
+
+    public List<string> Interests { get; set; } = [];
+}
