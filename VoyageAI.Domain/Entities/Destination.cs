ec11f7947945e26bc1b98eeb22a67d49b683b064
Saniya <saniyach334@gmail.com>
2026-07-28 17:17:19 +0800

    Add destination entity to domain layer

--- VoyageAI.Domain/Entities/Destination.cs
+++ VoyageAI.Domain/Entities/Destination.cs
@@ -2,9 +2,17 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace VoyageAI.Domain.Entities
+namespace VoyageAI.Domain.Entities;
+
+public class Destination
 {
-    internal class Destination
-    {
-    }
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public string Country { get; set; } = string.Empty;
+
+    public string Description { get; set; } = string.Empty;
+
+    public double Rating { get; set; }
 }
