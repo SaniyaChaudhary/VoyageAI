f10c6ec787d6a5595f7a82c74eb6846991a07321
Saniya <saniyach334@gmail.com>
2026-07-29 16:35:58 +0800

    Add AI service abstraction and OpenAI service

---
+++ VoyageAI.Application/Interfaces/IAIService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoyageAI.Application.Interfaces
+{
+    internal class IAIService
+    {
+    }
+}
---
+++ VoyageAI.Infrastructure/AI/OpenAIService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VoyageAI.Infrastructure.AI
+{
+    internal class OpenAIService
+    {
+    }
+}
